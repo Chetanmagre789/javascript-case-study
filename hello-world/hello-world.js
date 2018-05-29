f289d058bcd56716ad191c266752417b7ea15d0f
Chetanmagre789 <38548394+Chetanmagre789@users.noreply.github.com>
2018-05-29 12:47:04 +0530

    hello world

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
  function HelloWorld(){
     this.hello=function(){
         return 'Hello, World!';
     }
 };
 module.exports =HelloWorld;
-=======
-function HelloWorld() {
-	this.hello = function() {
-		return 'Hello, World!';
-	}
-}
 
-module.exports = HelloWorld;
->>>>>>> 23721c65e34e8eb7006cc2600f20718cef338cae
